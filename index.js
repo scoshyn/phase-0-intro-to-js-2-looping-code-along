d4415661ca9de859521b915c6ec2ad98f6411252
stephen oshyn <scoshyn@gmail.com>
2023-09-01 10:09:47 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solutions in this file
+let names = [];
+
+function writeCards(names, event) {
+    let messages = [];
+
+    for (let i = 0; i < names.length; i++) {
+        debugger;
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        debugger;
+    };
+    return messages
+};
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise")
+
+
+function countDown(startingNumber) {
+    //some code;
+
+    let i = startingNumber;
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    };
+};
+
+countDown(10);
